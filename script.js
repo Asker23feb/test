160b9702616be4548ebdb6f7523f452842538441
Asker Koshmuratov <Asker23feb@gmail.com>
2020-01-30 23:15:47 +0600

    change

---
+++ script.js
@@ -0,0 +1,22 @@
+$('#fullpage').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    menu: '#menu',
+    slidesNavigation: true,
+    navigation: true,
+});
+
+// $(document).ready(function() {
+//     $(".owl-carousel").owlCarousel({
+//         items: 1,
+//         nav: true,
+//         dots: true,
+//         loop: true,
+//         navText: ["<img src='./img/left.png'>", "<img src='./img/right.png'>"],
+//         animateOut: "fadeOut",
+//         animateIn: "fadeIn",
+//         autoplay: true,
+//         autoplayTimeout: 2000,
+//         autoplayHoverPause: true
+//     });
+// });
